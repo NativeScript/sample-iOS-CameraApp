1b1b080eecdfa7ac2a39a5aae75a355189f8fcb9
Panayot Cankov <Panayot.Cankov@telerik.com>
2015-05-13 11:52:15 +0300

    Update the project to 0.10

--- CameraApp/tns_modules/application/application-common.js
+++
@@ -1,8 +0,0 @@
-require("globals");
-exports.onLaunch = undefined;
-exports.onSuspend = undefined;
-exports.onResume = undefined;
-exports.onExit = undefined;
-exports.onLowMemory = undefined;
-exports.android = undefined;
-exports.ios = undefined;
--- CameraApp/tns_modules/ui/dialogs/dialogs-common.js
+++
@@ -1,6 +0,0 @@
-exports.STRING = "string", exports.PROMPT = "Prompt", exports.CONFIRM = "Confirm", exports.ALERT = "Alert", exports.LOGIN = "Login", exports.OK = "OK", exports.CANCEL = "Cancel";
-(function (InputType) {
-    InputType[InputType["PlainText"] = 0] = "PlainText";
-    InputType[InputType["Password"] = 1] = "Password";
-})(exports.InputType || (exports.InputType = {}));
-var InputType = exports.InputType;
